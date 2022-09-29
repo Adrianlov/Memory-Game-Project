2b73a0c42ef43c2af7040f957eb1288fc785576e
Adrianlov <74472740+Adrianlov@users.noreply.github.com>
2022-09-29 21:31:35 +1000

    Update storage.js

--- JavaScript/storage.js
+++ JavaScript/storage.js
@@ -3,5 +3,10 @@ let showClicks = document.getElementById('retriveClicks')
 
 let clicks = localStorage.getItem('clicks')
 
+let retriveClicks = JSON.parse(clicks)
+
+
+showClicks.textContent = 'With ' +  retriveClicks + ' Clicks'
+
 
 showClicks.textContent = 'With ' +  clicks + ' Clicks'
